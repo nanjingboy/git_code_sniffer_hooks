4e0db923555abbb9d645f58205143627f5926b26
Tom <hzlhu.dargon@gmail.com>
2013-04-23 22:23:21 +0800

    complete pre-receive hook for javascript code sniffer

--- common.py
+++ common.py
@@ -1,15 +1,35 @@
 #-*- coding:utf-8 -*-
 from os import system
-from sh import awk, grep, cat
+from sh import awk, grep, cat, mkdir, rm
+from config import config
+
+def create_tmp_dir():
+  tmp_dir = config.get("receive", "TMP_DIR")
+  if tmp_dir:
+    mkdir("-p", tmp_dir)
+
+  return tmp_dir
+
+def remove_tmp_dir():
+  tmp_dir = config.get("receive", "TMP_DIR")
+  if tmp_dir:
+    rm("-rf", tmp_dir)
 
 def get_commit_files(file_type):
   system("git diff --cached --name-status > /tmp/git_hook")
+  return _get_files(file_type, 2)
 
+def get_receive_files(rev_old, rev_new, file_type):
+  system("git diff-tree -r %s..%s > /tmp/git_hook" % (rev_old, rev_new))
+  return _get_files(file_type, 6)
+
+def _get_files(file_type, file_index):
   files = awk(
     grep(
-     cat("/tmp/git_hook"), "-P", "(A|M).*.%s$" % file_type,
-     _ok_code = [0, 1]
-    ), "{print $2}", _iter = True
+      cat("/tmp/git_hook"), "-P", "(A|M).*.%s$" % file_type,
+      _ok_code = [0, 1]
+    ),
+    "{print $%s}" % file_index, _iter = True
   )
 
   if not files:
--- javascript.py
+++ javascript.py
@@ -1,8 +1,10 @@
 #-*- coding:utf-8 -*-
 from config import config, base_path
-from common import get_commit_files
-from sh import jshint
+from common import get_commit_files, get_receive_files
+from common import create_tmp_dir, remove_tmp_dir
+from sh import jshint, tail
 from termcolor import colored
+from os import system
 
 def get_commit_errors():
   checkable = config.getboolean("commit", "CHECK_JAVASCRIPT")
@@ -26,6 +28,35 @@ def get_commit_errors():
   if errors:
     errors = colored(
       "There are some errors in below javascript files:\n\n", "magenta"
-    ) + "\n".join(errors)
+    ) + "\n".join(errors).strip("\n")
 
   return errors
+
+def get_receive_errors(rev_old, rev_new):
+  checkable = config.getboolean("receive", "CHECK_JAVASCRIPT")
+  if not checkable:
+    return None
+
+  files = get_receive_files(rev_old, rev_new, "js")
+
+  if not files:
+    return None
+
+  errors = []
+  tmp_dir = create_tmp_dir()
+  for path in files:
+    system("git show %s:%s > %s" % (rev_new, path, tmp_dir + path))
+
+    file_error = tail(
+      jshint(
+        "--config",
+        "%s/configs/jshint.json" % base_path,
+        tmp_dir + path, _ok_code = [0, 2]
+      ), "-n", 1
+    )
+
+    if file_error:
+      errors.append(colored(path + " " + str(file_error).strip("\n"), "red"))
+
+  remove_tmp_dir()
+  return "\n".join(errors)
