a789240a2243cfc6995cb057298c78d454595866
Tom <hzlhu.dargon@gmail.com>
2013-04-24 09:32:01 +0800

    complete pre-receive hook for php code sniffer

--- php.py
+++ php.py
@@ -1,8 +1,10 @@
 #-*- coding:utf-8 -*-
 from config import config
-from common import get_commit_files
+from common import get_commit_files, get_receive_files
+from common import create_tmp_dir, remove_tmp_dir
 from sh import phpcs
 from termcolor import colored
+from os import system
 
 def get_commit_errors():
   checkable = config.getboolean("commit", "CHECK_PHP")
@@ -30,3 +32,29 @@ def get_commit_errors():
     ) + "\n".join(errors)
 
   return errors
+
+def get_receive_errors(rev_old, rev_new):
+  checkable = config.get("receive", "CHECK_PHP")
+  if not checkable:
+    return None
+
+  files = get_receive_files(rev_old, rev_new, "php")
+  if not files:
+    return None
+
+  errors = []
+  tmp_dir = create_tmp_dir()
+  for path in files:
+    system("git show %s:%s > %s" % (rev_new, path, tmp_dir + path))
+    file_errors = phpcs(
+      "--report=summary", tmp_dir + path, _iter = True, _ok_code = [0, 1]
+    )
+    if file_errors:
+      file_errors = [str(file_error) for file_error in file_errors]
+      error_info = file_errors[-3].split(" ")
+      error = colored("%s error(s)" % error_info[3], "red")
+      warning = colored("%s warning(s)" % error_info[6], "yellow")
+      errors.append(path + "    " + error + "  " + warning)
+
+  return "\n".join(errors)
+
