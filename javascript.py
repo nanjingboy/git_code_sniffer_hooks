fe6381beb358b2ddd3059571eb21fd11820097a5
Tom <hzlhu.dargon@gmail.com>
2013-06-13 14:39:52 +0800

    update js check logic

--- javascript.py
+++ javascript.py
@@ -1,5 +1,5 @@
 #-*- coding:utf-8 -*-
-from sh import jshint, tail
+from commands import getoutput
 from termcolor import colored
 from config import base_path
 from common import get_commit_errors as get_javascript_commit_errors
@@ -9,15 +9,8 @@ def get_commit_errors():
   return get_javascript_commit_errors("js", _get_commit_file_error)
 
 def _get_commit_file_error(path):
-  error =  jshint(
-    "--config",
-    "%s/configs/jshint.json" % base_path,
-    path,  _ok_code = [0, 2]
-  )
-  if error:
-    return colored(error, "red")
-
-  return None
+  error = _get_error(path)
+  return colored(error, "red") if error else None
 
 def get_receive_errors(rev_old, rev_new):
   return get_javascript_receive_errors(
@@ -25,13 +18,11 @@ def get_receive_errors(rev_old, rev_new):
   )
 
 def _get_receive_file_error(path):
-  error = tail(
-    jshint(
-      "--config", "%s/configs/jshint.json" % base_path,
-      path, _ok_code = [0, 2]
-    ), "-n", 1
-  )
-  if error:
-    return  "     " + colored(str(error).strip("\n"), "red")
+  errors = _get_error(path).split("\n")
+  return "     " + colored(len(errors), "red") if errors else None
 
-  return None
+def _get_error(path):
+  return  getoutput(
+    "%s/jshint.js --config %s/configs/jshint.json %s"  %
+    (base_path, base_path, path)
+  )
