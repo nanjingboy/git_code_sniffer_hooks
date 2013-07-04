aa1c3ee1a5fc230e599cee0a42d3723f8502ccff
Tom <hzlhu.dargon@gmail.com>
2013-07-04 08:48:26 +0800

    remove unuseful code

--- phpcs/Sniffs/NamingConventions/ValidVariableNameSniff.php
+++ phpcs/Sniffs/NamingConventions/ValidVariableNameSniff.php
@@ -7,65 +7,6 @@ class PHPCS_Sniffs_NamingConventions_ValidVariableNameSniff extends Zend_Sniffs_
 {
     protected function processVariable(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        $tokens  = $phpcsFile->getTokens();
-        $varName = ltrim($tokens[$stackPtr]['content'], '$');
-        $phpReservedVars = array(
-            '_SERVER',
-            '_GET',
-            '_POST',
-            '_REQUEST',
-            '_SESSION',
-            '_ENV',
-            '_COOKIE',
-            '_FILES',
-            'GLOBALS',
-        );
-
-        // If it's a php reserved var, then its ok.
-        if (in_array($varName, $phpReservedVars) === true) {
-            return;
-        }
-
-        $objOperator = $phpcsFile->findNext(array(T_WHITESPACE), ($stackPtr + 1), null, true);
-        if ($tokens[$objOperator]['code'] === T_OBJECT_OPERATOR) {
-            // Check to see if we are using a variable from an object.
-            $var = $phpcsFile->findNext(array(T_WHITESPACE), ($objOperator + 1), null, true);
-            if ($tokens[$var]['code'] === T_STRING) {
-                // Either a var name or a function call, so check for bracket.
-                $bracket = $phpcsFile->findNext(array(T_WHITESPACE), ($var + 1), null, true);
-
-                if ($tokens[$bracket]['code'] !== T_OPEN_PARENTHESIS) {
-                    $objVarName = $tokens[$var]['content'];
-
-                    // There is no way for us to know if the var is public or private,
-                    // so we have to ignore a leading underscore if there is one and just
-                    // check the main part of the variable name.
-                    $originalVarName = $objVarName;
-                    if (substr($objVarName, 0, 1) === '_') {
-                        $objVarName = substr($objVarName, 1);
-                    }
-                }
-            }
-        }
-
-        // There is no way for us to know if the var is public or private,
-        // so we have to ignore a leading underscore if there is one and just
-        // check the main part of the variable name.
-        $originalVarName = $varName;
-        if (substr($varName, 0, 1) === '_') {
-            $objOperator = $phpcsFile->findPrevious(array(T_WHITESPACE), ($stackPtr - 1), null, true);
-            if ($tokens[$objOperator]['code'] === T_DOUBLE_COLON) {
-                // The variable lives within a class, and is referenced like
-                // this: MyClass::$_variable, so we don't know its scope.
-                $inClass = true;
-            } else {
-                $inClass = $phpcsFile->hasCondition($stackPtr, array(T_CLASS, T_INTERFACE));
-            }
-
-            if ($inClass === true) {
-                $varName = substr($varName, 1);
-            }
-        }
     }
 
     protected function processMemberVar(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
@@ -91,6 +32,10 @@ class PHPCS_Sniffs_NamingConventions_ValidVariableNameSniff extends Zend_Sniffs_
                 return;
             }
         }
+    }
+
+    protected function processVariableInString(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
     }
 }
 ?>
